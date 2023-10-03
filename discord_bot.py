fbc23299abf3dacd6ebb73159f8e3f28306e876b
sachaCOOL <97050269+sachaCOOL@users.noreply.github.com>
2023-10-03 20:02:04 +0200

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -2,6 +2,7 @@ import os
 import discord
 import requests
 import random
+from discord.ext import commands
 
 pfcLi = ['Pierre', 'Feuille', 'Ciseaux']
 randSi = random.choice(pfcLi)
@@ -9,7 +10,7 @@ randSi = random.choice(pfcLi)
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 def get_crypto_prices():
     crypto_list = ["bitcoin", "ethereum", "dogecoin", "litecoin", "polygon", "ripple", "tether", "solana"]
@@ -30,13 +31,13 @@ def get_crypto_prices():
 
     return prices
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.startswith('$hello'):
@@ -63,8 +64,51 @@ async def on_message(message):
             price_message = "\n".join(f"{crypto.capitalize()}: ${price}" for crypto, price in prices.items())
             await message.channel.send(f"Real-time cryptocurrency prices:\n{price_message}")
 
+@bot.command()
+async def play_youtube(ctx, channel_name: str, url: str):
+    # Check if the user is in a voice channel
+    if not ctx.author.voice:
+        await ctx.send("You are not in a voice channel.")
+        return
+
+    # Find the specified voice channel by name
+    target_channel = discord.utils.get(ctx.guild.voice_channels, name=channel_name)
+
+    if not target_channel:
+        await ctx.send(f"Voice channel '{channel_name}' not found.")
+        return
+
+    # Get the voice channel that the user is in
+    author_vc = ctx.author.voice.channel
+
+    # Check if the user is in the target voice channel
+    if author_vc != target_channel:
+        await ctx.send("You can only play in your own voice channel.")
+        return
+
+    # Join the user's voice channel
+    voice_channel = await author_vc.connect()
+
+    # Check if the bot is already playing in a voice channel
+    if ctx.voice_client.is_playing():
+        await ctx.send("The bot is already playing something.")
+        return
+
+    # Check if the URL is a valid YouTube video URL
+    if not url.startswith("https://www.youtube.com/watch?v="):
+        await ctx.send("Invalid YouTube video URL.")
+        return
+
+    try:
+        # Stream the audio from the YouTube video
+        ctx.voice_client.stop()  # Stop any previous playback
+        ctx.voice_client.play(discord.FFmpegPCMAudio(url))
+        await ctx.send(f"Now playing: {url}")
+    except Exception as e:
+        await ctx.send(f"An error occurred: {e}")
+
 # On récupère notre token discord dans l'env de Railway
 bot_token = os.environ.get("DISCORD_BOT_TOKEN")
 
 # Pour lancer le bot
-client.run(bot_token)
+bot.run(bot_token)
