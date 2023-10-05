47d7dc4321d07d005f4e28f8281603e36a12942a
sachaCOOL <97050269+sachaCOOL@users.noreply.github.com>
2023-10-05 17:12:33 +0200

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -63,54 +63,7 @@ async def on_message(message):
         if prices:
             price_message = "\n".join(f"{crypto.capitalize()}: ${price}" for crypto, price in prices.items())
             await message.channel.send(f"Real-time cryptocurrency prices:\n{price_message}")
-
-@bot.command()
-async def play_youtube(ctx, channel_name: str, url: str):
-    # Check if the user is in a voice channel
-    if not ctx.author.voice:
-        await ctx.send("You are not in a voice channel.")
-        return
-
-    # Find the specified voice channel by name
-    target_channel = discord.utils.get(ctx.guild.voice_channels, name=channel_name)
-
-    if not target_channel:
-        await ctx.send(f"Voice channel '{channel_name}' not found.")
-        return
-
-    # Get the voice channel that the user is in
-    author_vc = ctx.author.voice.channel
-
-    # Debugging: Print the received channel name and URL
-    print(f"Received channel_name: {channel_name}")
-    print(f"Received URL: {url}")
-
-    # Check if the user is in the target voice channel
-    if author_vc != target_channel:
-        await ctx.send("You can only play in your own voice channel.")
-        return
-
-    # Join the user's voice channel
-    voice_channel = await author_vc.connect()
-
-    # Check if the bot is already playing in a voice channel
-    if ctx.voice_client.is_playing():
-        await ctx.send("The bot is already playing something.")
-        return
-
-    # Check if the URL is a valid YouTube video URL
-    if not url.startswith("https://www.youtube.com/watch?v="):
-        await ctx.send("Invalid YouTube video URL.")
-        return
-
-    try:
-        # Stream the audio from the YouTube video
-        ctx.voice_client.stop()  # Stop any previous playback
-        ctx.voice_client.play(discord.FFmpegPCMAudio(url))
-        await ctx.send(f"Now playing: {url}")
-    except Exception as e:
-        await ctx.send(f"An error occurred: {e}")
-
+            
 
 # On récupère notre token discord dans l'env de Railway
 bot_token = os.environ.get("DISCORD_BOT_TOKEN")
